4468547e70d1d24a91dee47994f73cafa576274e
Dahye Kim <dahyek24@gmail.com>
2018-02-12 10:12:06 +0900

    Create fbconfig.py

---
+++ app/fbconfig.py
@@ -0,0 +1,16 @@
+# access_token을 관리하는 방식을 생각해봐야 겠다
+access_token = "EAAY5OSYVOZAMBAEpv0GuuT21gjtCagFhPEb5UAIFuZCR5J1xdItGBvaH2nmENhnqfaZAyClCWbtiUxBC9EXyWb4kHSuFk0o8TL6ZB4a658e1qRypMYpiGP65ZAKWbeLGA0IHvde7SiZCBFEBz6O9biRZBCV6sFU8CcZD"
+# 각 학교의 community의 id를 관리
+community_list = {
+    'kyunghee': [
+        {
+            'id': '482012061908784',
+            'name': 'Kyunghee bamboo grove'  # 경희대학교 대나무 숲
+        },
+        {
+            'id': '449006815233880',
+            'name': '경희대학교 - 국제캠 대신 전해드립니다',
+            'url': 'https://www.facebook.com/kyungheedaesin/'
+        }
+    ],
+}
